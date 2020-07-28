28d44375020e45690dea3bb80feba88df97f0be0
miguel <miguel.rueda@outlook.com>
2020-07-27 19:28:15 -0500

    Module 2 finished

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,21 @@ import React, { useState } from "react";
 const InputElement = () => {
 
     const [inputText, setInputText] = useState("");
+    const [historyList, setHistoryList] = useState([]);
 
     return <div><input placeholder="Enter some text"
         onChange={(e) => {
             setInputText(e.target.value);
+            setHistoryList([...historyList, e.target.value])
         }} /><br />
         {inputText}
+        <hr />
+        <br />
+        <ul>
+            {historyList.map((h) => {
+                return <div>{h}</div>}
+            )}
+        </ul>
     </div>
 };
 
